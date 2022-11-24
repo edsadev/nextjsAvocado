e44e1da32f3210df912dc6ee6235c181af6a5e69
edsadev <esala.094@gmail.com>
2022-11-24 16:49:56 -0500

    Normalize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
 // getStaticProps
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const response = await fetch('https://platzi-avo.vercel.app/api/avo')
   const { data }: TAPIAvoResponse = await response.json()
 
@@ -15,6 +15,17 @@ export const getServerSideProps = async () => {
     },
   }
 }
+
+// export const getServerSideProps = async () => {
+//   const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+//   const { data }: TAPIAvoResponse = await response.json()
+
+//   return {
+//     props: {
+//       productList: data,
+//     },
+//   }
+// }
 
 const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
